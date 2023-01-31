5b92d10755dacd53780663f3726ad9e96f19d9e6
SpiritLord <kristiqn062001@abv.bg>
2023-01-31 04:19:34 +0200

    some changes for gallery page

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,35 +1,46 @@
 <script>
-    import Gallery from 'svelte-gallery';
+// @ts-nocheck
+    // import { onMount } from 'svelte';
+    
+    let images = [];
+    
+    for(let i = 50; i<80; i++){
+        images.push({
+                alt: "",
+                src: `https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(${i}).webp`
+        })
+    }
   
-    const images = [
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
-      { src: 'https://source.unsplash.com/random', width: 600, height: 400 },
+    // onMount(async () => {
+    // //   images = await fetch('/api/images').then(res => res.json());
 
-    ];
+    //     for(let i = 70; i<80; i++){
+    //         images.push({
+    //             alt: "",
+    //             src: `https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(${i}).webp`
+    //         })
+    //     }
+    // });
 </script>
 
-<Gallery {images} />
+<div class="bg-gray-900 select-none">
+    <div class="w-full pt-8">
+        <p class="m-auto w-fit text-gray-400  text-4xl p-5 bg-gray-800 block">Image Gallery</p>
+    </div>
+    <section class="overflow-hidden text-gray-700 ">
+        <div class="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
+            <div class="flex flex-wrap -m-1 md:-m-2">
+                {#each images as image}
+                    <div class="flex flex-wrap w-1/3">
+                        <div class="w-full p-1 md:p-2">
+                            <img class="block object-cover object-center w-full h-full rounded-lg"
+                            loading="lazy"
+                            alt={image.alt}
+                            src={image.src}>
+                        </div>
+                    </div>
+                {/each}
+            </div>
+        </div>
+    </section>
+</div>
