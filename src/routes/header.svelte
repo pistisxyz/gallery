a949a554fba2a272c88397b027e4edad57a43107
SpiritLord <kristiqn062001@abv.bg>
2023-01-31 04:22:23 +0200

    added upload page some navbar changes

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,4 +1,6 @@
 <script>
+// @ts-nocheck
+
     import piko from '$lib/images/pikologo.svg';
     import userPlaceholder from '$lib/images/userPlaceholder.png'
 
@@ -12,6 +14,11 @@
 	function toggleMobile() {
 		mobileMenu = !mobileMenu;
 	}
+
+    function signout() {
+        toggleProfile();
+        loggedIn = !loggedIn
+    }
 
     function toggleProfile(){
         ProfileMenu= !ProfileMenu
@@ -72,8 +79,8 @@
             </div>
             {#if ProfileMenu}
                 <div class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                <a on:click={toggleProfile} href="/profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
-                <a on:click={toggleProfile} href="#logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
+                 <a on:click={toggleProfile} href="/profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
+                 <a on:click={signout} href="#logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Sign out</a>
                 </div>
             {/if}
           </div>
---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+    // @ts-nocheck
+
+    import { PUBLIC_UPLOAD_URL } from '$env/static/public';
+
+    let fileList = [];
+
+    let fileInput;
+
+    let uploading = false;
+    
+    function move_handle(){
+        fileList = [];
+        for(var i = 0; i < fileInput.files.length; i++){
+            fileList.push(fileInput.files[i]);
+        }
+    }
+
+    function clear_fileInput(){
+        try{
+            fileInput.value = '';
+            if(fileInput.value){
+                fileInput.type = "text";
+                fileInput.type = "file";
+            }
+        }catch(e){}
+    }
+
+    async function upload(){
+        if(uploading) return alert("you can't start 2 uploading streams at the same time!");
+
+        clear_fileInput()
+        uploading = true;
+
+        for(let  i = 0; i < fileList.length; i++){
+            let data = new FormData()
+            data.append("file", fileList[i]);
+            data.append();
+
+            let result = fetch(PUBLIC_UPLOAD_URL, {
+                method: 'POST',
+                headers: {
+                    'Authorization': "Bearer " + "$token"
+                },
+                body: data
+            }).await
+
+            console.log(result)
+
+        }
+
+        uploading = false; 
+        
+    }
+    
+
+</script>
+
+
+<div class=" flex justify-center m-auto items-center h-[calc(100vh-400px)]">
+    <form id="container" class=" bg-netural-300 w-96 p-6 shadow-lg rounded-md border border-neutral-400 border-dashed hover:border-neutral-200 text-center" on:submit|preventDefault={upload}>
+        <label class=" text-center block mb-2 text-base text-indigo-500 font-semibold" for="uploadarea">Choose or Drop photos</label>
+        <hr class="mb-2">
+        <input class="bg-white rounded-md" type="file" id="uploadarea" multiple accept="image/*"  bind:this={fileInput} on:change={move_handle}/>
+        <hr class="mt-2 opacity-0">
+        <input id="submitter" class="p-1 bg-white w-full text-base text-indigo-500 font-semibold rounded-md hover:bg-neutral-200" value="Upload" type="submit">
+    </form>  
+</div>
+
