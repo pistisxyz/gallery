9f2fe750caf994094c5a96b9d0e0386509a65f7f
SpiritLord <kristiqn062001@abv.bg>
2023-06-08 00:44:28 +0300

    add version number to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
+<script>
+    let version = "version 0.73 beta"
+</script>
+
 <div class="text-gray-400 h-screen bg-white">
     <div class="py-20 mb-8" style="background: linear-gradient(90deg, #764ba2 0%, #667eea 100%)">
       <div class="container mx-auto px-6">
         <h2 class="text-5xl font-bold mb-2 text-white">
           De La Website
         </h2>
+        <div>
+            {version}
+        </div>
       </div>
     </div>
     <div class="py-20" style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)">
